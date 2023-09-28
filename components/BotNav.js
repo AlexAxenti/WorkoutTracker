b63294a096e8f3ac87c0d2e6117a3431648582ee
AlexAxenti <alex.axenti@gmail.com>
2023-09-27 22:33:10 -0400

    Seperated botnav to a component

---
+++ components/BotNav.js
@@ -0,0 +1,20 @@
+import { Text, View, TouchableOpacity } from 'react-native';
+import React from 'react';
+import stylesSheet from '../styles.js'
+
+const BotNav = (props) => {
+  return (
+    <View style={styles.botNav}>
+      <TouchableOpacity style={styles.botNavButton} onPress={() => props.navigation.navigate('LogList')}>
+        <Text>Log List</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.botNavButton} onPress={() => props.navigation.navigate('RoutineList')}>
+        <Text>RoutineList</Text>
+      </TouchableOpacity>
+    </View>
+  )
+};
+
+const styles = stylesSheet;
+
+export default BotNav
